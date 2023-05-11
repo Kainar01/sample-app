ba21757724f6aad5edcb14fa6e272752b6c7cf41
Kainar Masujima <masujimk@gmail.com>
2023-05-11 11:28:47 +0600

    fix: config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     'apps/server/**/*.ts': 'eslint --fix --config apps/server/.eslintrc.json',
     'libs/util/logger/**/*.ts': 'eslint --fix --config libs/util/logger/.eslintrc.json',
-    'libs/util/nestjs/**/*.ts': 'eslint --fix --config libs/util/nestjs/.eslintrc.json',
+    'libs/util/nestjs/logger/**/*.ts': 'eslint --fix --config libs/util/nestjs/logger/.eslintrc.json',
     '*.{json,md}': 'prettier --write'
 }
