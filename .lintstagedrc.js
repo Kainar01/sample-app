ffd049ea61ec843a306af36df39af93c898cb563
Kainar Masujima <masujimk@gmail.com>
2023-05-17 21:20:35 +0600

    build: add jest in lintstage

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'apps/server/**/*.ts': 'eslint --fix --config apps/server/.eslintrc.json',
+    'apps/server/**/*.ts': ['eslint --fix --config apps/server/.eslintrc.json', 'yarn test:server --bail --findRelatedTests'],
     'libs/util/logger/**/*.ts': 'eslint --fix --config libs/util/logger/.eslintrc.json',
     'libs/nestjs/redis/**/*.ts': 'eslint --fix --config libs/nestjs/redis/.eslintrc.json',
     'libs/util/nestjs/logger/**/*.ts': 'eslint --fix --config libs/util/nestjs/logger/.eslintrc.json',
