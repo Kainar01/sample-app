33e2024e05252c33d2a0884aff936d9cde27f769
Kainar Masujima <masujimk@gmail.com>
2023-05-19 15:30:44 +0600

    refactor: remove empty space

--- apps/server/src/core/health/health.module.ts
+++ apps/server/src/core/health/health.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 
-
 import { HealthIndicatorService } from './health-indicator.service';
 import { HealthController } from './health.controller';
 import { PrismaModule } from '../../prisma';
